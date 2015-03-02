569315623bada63507b9f01d9963b9aebcc07177
jonathoncihlar <jonathon.cihlar@academicapproach.com>
2015-03-02 13:57:29 -0600

    KISS Template Project
    Created KISS template project file and template.c C file.

---
+++ Template/template.c
@@ -0,0 +1,21 @@
+/**
+ * template.c
+ * This C file contains a template for programs for 
+ * the Senn Coding Club - Robotics Team.
+ * These comments should be replaced with comments that describe the 
+ * overall purpose and function of the particular program.
+ * (c) 2015 - Nicholas Senn High School Coding Club - Robotics Team
+ * Faculty Advisor: J. Cihlar
+ */
+
+
+	/** 
+	 * int main() 
+	 * Insert a description of the key aspects of the main
+	 * function instead of this comment.
+	 */
+int main () {
+	
+			// return 0 if everything is successful
+		return 0;
+}
